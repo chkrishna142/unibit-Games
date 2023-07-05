32a9e2a88cf31573032532d39b910ba108f843ba
chkrishna142 <chkrishna142@gmail.com>
2023-07-05 07:41:39 +0530

    done with all outputs

--- assigment.js
+++ assigment.js
@@ -25,3 +25,69 @@ console.log("First Combination For “4” : "); console.log(result1);
 
 const mergeArr = result1.flat().sort((a, b) => a - b);
 console.log("Merge Into a single Array : "); console.log(mergeArr);
+
+
+function equalArrayCheck(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function duplicate(presentSeq, result) {
+    for (const arr of result) {
+        if (equalArrayCheck(presentSeq, arr)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function backtrack(mergeArr, doubletarget, ind, presentSeq, result) {
+    if (doubletarget === 0 && !duplicate(presentSeq, result)) {
+        result.push([...presentSeq]);
+        return;
+    }
+
+    if (ind === mergeArr.length || doubletarget < 0) {
+        return;
+    }
+
+    for (let i = ind; i < mergeArr.length; i++) {
+        presentSeq.push(mergeArr[i]);
+        backtrack(mergeArr, doubletarget - mergeArr[i], i + 1, presentSeq, result);
+        presentSeq.pop();
+    }
+}
+
+
+function findSubseqWithSum(mergeArr, doubletarget) {
+    const result = [];
+    const presentSeq = [];
+    backtrack(mergeArr, doubletarget, 0, presentSeq, result);
+    return result;
+}
+
+
+
+
+
+
+
+
+const doubletarget = 8;
+const subsequences = findSubseqWithSum(mergeArr, doubletarget);
+
+console.log("Second Combination For “8” : ");
+console.log("[");
+subsequences.forEach(subsequence => {
+    console.log(subsequence);
+});
+console.log("]");
